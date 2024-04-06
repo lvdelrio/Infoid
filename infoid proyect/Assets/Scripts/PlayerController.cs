fd38811213b95af56b17f57eb3160277116f1fb2
AgBallivian <agballivian@miuandes.cl>
2024-04-06 18:47:47 -0300

    Player controller
    Sliding funcional, hay que arreglar el salto

--- infoid proyect/Assets/Scripts/PlayerController.cs
+++ infoid proyect/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 10f;
     public float moveSpeed = 5f;
+    public float jumpAngle = 45f;
+    public float slidingSpeed = 2f;
+    public float wallSlideRotation = 45f;
     public Rigidbody2D rb;
-    private bool isWallRunning = false;
+    private bool isWallSliding = false;
+    private bool isTouchingWall = false;
+    private float wallDirection = 0f;
 
     public Collider2D verticalCollider;
     public Collider2D horizontalCollider;
@@ -13,7 +18,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
         verticalCollider.enabled = true;
         horizontalCollider.enabled = false;
     }
@@ -21,39 +26,88 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        Debug.Log("Touch: " + isTouchingWall + " Slide: " + isWallSliding);
+        if (isTouchingWall)
         {
-            if (isWallRunning)
-            {
-                
-                rb.velocity = new Vector2(rb.velocity.x * 0.5f, jumpForce); 
-                isWallRunning = false; 
-            }
+            isWallSliding = true;
+            horizontalCollider.enabled = true;
+            verticalCollider.enabled = false;
+        }
+        else
+        {
+            isWallSliding = false;   
         }
 
         
-        verticalCollider.enabled = !isWallRunning;
-        horizontalCollider.enabled = isWallRunning;
+        if (isWallSliding)
+        {
+            rb.velocity = new Vector2(0f, Mathf.Clamp(rb.velocity.y, -slidingSpeed, float.MaxValue));
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                float jumpAngleRadians = jumpAngle * Mathf.Deg2Rad;
+                Vector2 jumpDirection = new Vector2(Mathf.Cos(jumpAngleRadians), Mathf.Sin(jumpAngleRadians));
+
+                if (wallDirection < 0)
+                {
+                    jumpDirection.x *= -1;
+                }
+
+                horizontalCollider.enabled = false;
+                verticalCollider.enabled = true;
+                
+                isTouchingWall = false;
+                isWallSliding = false;
+
+                // rb.velocity = new Vector2(0f, jumpDirection.y * jumpForce);
+                rb.AddForce(jumpDirection.normalized * jumpForce, ForceMode2D.Impulse);
+                transform.rotation = Quaternion.identity;
+
+                
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        }
+
+
+
+        if (!isWallSliding)
+        {
+            horizontalCollider.enabled = false;
+            verticalCollider.enabled = true;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.CompareTag("Wall"))
         {
-            isWallRunning = true; 
-            rb.gravityScale = 0.5f; 
+            isTouchingWall = true;
+            rb.gravityScale = 0.8f;
+
+            wallDirection = collision.GetContact(0).normal.x;
+
+            if (wallDirection > 0)
+            {
+                Debug.Log("Left");
+                // transform.rotation = Quaternion.Euler(0f, 0f, -wallSlideRotation);
+            }
+            else if (wallDirection < 0)
+            {
+                Debug.Log("Right");
+                // transform.rotation = Quaternion.Euler(0f, 0f, wallSlideRotation);
+            }
         }
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.CompareTag("Wall"))
         {
-            isWallRunning = false;
-            rb.gravityScale = 1; 
+            rb.gravityScale = 1f;
+            transform.rotation = Quaternion.identity;
         }
     }
 }
