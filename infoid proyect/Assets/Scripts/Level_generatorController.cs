026832389586c0718dbbe538e7e0e3fa89059776
lvdelrio <lvdelrio@miuandes.cl>
2024-04-13 16:37:54 -0400

    fix initial walls are prefab and are destroy eventually
    all the walls it will destroy after 50f unit from player

--- infoid proyect/Assets/Scripts/Level_generatorController.cs
+++ infoid proyect/Assets/Scripts/Level_generatorController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Level_generatorController : MonoBehaviour
@@ -10,28 +11,50 @@ public class Level_generatorController : MonoBehaviour
     [SerializeField] private PlayerController player; 
 
     private Vector3 lastposition;
+    private List<Transform> activeSegments = new List<Transform>();
+    public Transform level_segment1_start;
+    private Transform instantiatedSegmentStart;
+
     private void Awake() {
-        lastposition =level_segment_start.Find("end_segment").position;
-        for (int i=0; i<level_segment.Count;i++){
+        instantiatedSegmentStart = Instantiate(level_segment1_start, new Vector3(0f,0f,0f), Quaternion.identity);
+        lastposition = instantiatedSegmentStart.Find("end_segment").position;
+        for (int i=0; i<level_segment.Count; i++){
             spawn_segment();
         }
         
     }
     
     private void Update() {
+        if (instantiatedSegmentStart != null && Vector3.Distance(player.GetPosition(), instantiatedSegmentStart.position) > PLAYER_DISTANCE) {
+        Destroy(instantiatedSegmentStart.gameObject);
+        instantiatedSegmentStart = null;
+        Debug.Log("Destroying starting stage segment");
+        }
         if(Vector3.Distance(player.GetPosition(), lastposition) < PLAYER_DISTANCE) {
             spawn_segment();
         }
+        DestroyOldSegments();
+
     }
 
     private void spawn_segment(){
         Transform chosenSegmentPart = level_segment[Random.Range(0,level_segment.Count)];
         Transform lastlevelpartTransform = spawn_segments(chosenSegmentPart, lastposition);
         lastposition = lastlevelpartTransform.Find("end_segment").position;
+        activeSegments.Add(lastlevelpartTransform);
     }
 
      private Transform spawn_segments(Transform segment_part, Vector3 spawn_position){
         Transform levelpartTransform = Instantiate(segment_part, spawn_position, Quaternion.identity);
         return levelpartTransform;
     } 
+    private void DestroyOldSegments() {
+        for (int i = activeSegments.Count - 1; i >= 0; i--) {
+            if (Vector3.Distance(player.GetPosition(), activeSegments[i].position) > PLAYER_DISTANCE) {
+                Destroy(activeSegments[i].gameObject);
+                activeSegments.RemoveAt(i);  
+            }
+        }
+    }
+
 }
