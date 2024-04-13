0241a3d5ea24019101da38053b5edc8db64943de
AgBallivian <agballivian@miuandes.cl>
2024-04-13 18:22:25 -0400

    Bug fix: movimiento del jugador arrelgado ver1

--- infoid proyect/Assets/Scripts/PlayerController.cs
+++ infoid proyect/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 7f;
     public float distance;
     public float FallingConst = 5f;
-    public float jumpAngle = 45f;
-    public float slidingSpeed = 2f;
-    public float wallSlideRotation = 45f;
+    
     public Rigidbody2D rb;
     public Camera camera;
     public Collider2D verticalCollider;
@@ -18,10 +16,13 @@ public class PlayerController : MonoBehaviour
     private float wallDirection = 0f;
 
     [Header("Wall Jumping System")]
-    public Trasnform wallCheck;
     bool isTouchingWall = false;
     bool isWallSliding = false;
     public float wallSlidingSpeed;
+    public float wallJumpDuration;
+    public float jumpAngle = 45f;
+    public float wallJumpForce;
+    bool wallJumping;
     
 
     void Start()
@@ -38,6 +39,8 @@ public class PlayerController : MonoBehaviour
         float moveInputy = Input.GetAxis("Vertical");
         Vector2 moveInput = new Vector2(moveInputx, moveInputy).normalized;
 
+        // isTouchingWall = Physics2D.OverlapBox(wallCheck.position, new Vector2(0.15f, 0.2f), LayerMask.GetMask("Wall"));
+
         if (isTouchingWall)
         {
             isWallSliding = true;
@@ -47,36 +50,37 @@ public class PlayerController : MonoBehaviour
             isWallSliding = false;   
         }
 
-        if (isTouchingWall)
+        if (isWallSliding)
         {
-            rb.velocity = new Vector2(0f, Mathf.Clamp(rb.velocity.y, -slidingSpeed, float.MaxValue));
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
             transform.rotation = Quaternion.Euler(0, 0, -90);
             transform.localScale = new Vector3(7, -7, 1);
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                float jumpAngleRadians = jumpAngle * Mathf.Deg2Rad;
-                Vector2 jumpDirection = new Vector2(Mathf.Cos(jumpAngleRadians), Mathf.Sin(jumpAngleRadians));
-
-                if (wallDirection < 0)
-                {
-                    jumpDirection.x *= -1;
-                }
-
-                isTouchingWall = false;
-                isWallSliding = false;
-
-                rb.AddForce(jumpDirection.normalized * jumpForce, ForceMode2D.Impulse);
-                transform.rotation = Quaternion.identity;
+                Jump();
             }
-        }
-        else
+        } else
         {
             rb.velocity = new Vector2(moveInputx * moveSpeed, moveInputy != 0 ? moveInputy * moveSpeed : -FallingConst);
 
             transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.localScale = new Vector3(7, 7, 1);
         }
+
+        if(wallJumping)
+        {
+            float jumpAngleRadians = jumpAngle * Mathf.Deg2Rad;
+            Vector2 jumpDirection = new Vector2(Mathf.Cos(jumpAngleRadians), Mathf.Sin(jumpAngleRadians));
+
+            if (wallDirection < 0)
+            {
+                jumpDirection.x *= -1;
+            }
+            // rb.velocity = new Vector2(wallJumpForce.x * wallDirection, wallJumpForce.y);
+            rb.velocity = new Vector2(jumpDirection.x * wallJumpForce, jumpDirection.y * wallJumpForce);
+        }
+
 
         float currentYPosition = transform.position.y;
 
@@ -93,13 +97,27 @@ public class PlayerController : MonoBehaviour
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
     }
 
+    void Jump()
+    {
+        wallJumping = true;
+        isTouchingWall = false;
+        Invoke("StopWallJump", wallJumpDuration);
+
+
+        // rb.AddForce(jumpDirection.normalized * jumpForce, ForceMode2D.Impulse);
+        // transform.rotation = Quaternion.identity;
+    }    
+
+    void StopWallJump()
+    {
+        wallJumping = false;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
             isTouchingWall = true;
             animator.SetBool("onWall", true);
-            rb.gravityScale = 1f;
 
             wallDirection = collision.GetContact(0).normal.x;
 
@@ -118,7 +136,6 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            rb.gravityScale = 1.5f;
             transform.rotation = Quaternion.identity;
             animator.SetBool("onWall", false);
         }
