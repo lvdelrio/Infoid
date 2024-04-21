bdf707c0b61222731406046350fd428a3852c6ba
lvdelrio <lvdelrio@miuandes.cl>
2024-04-21 17:27:06 -0400

    fix position grappler

--- infoid proyect/Assets/Scripts/grappler.cs
+++ infoid proyect/Assets/Scripts/grappler.cs
@@ -1,30 +1,34 @@
+
 using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
-    public Transform player;
-    private bool hasInteracted = false;  // Flag to check if interaction has occurred
+    public GameObject player;
+    private bool hasInteracted = false;
+    public float offSet = 5;
 
+    private void start(){
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update(){
+        Vector3 newPosition = transform.position;
+        newPosition.y = player.transform.position.y + offSet;
+        transform.position = newPosition;
+        Debug.Log("ola " + transform.position);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if already interacted or if the colliding object is not an enemy
         if (hasInteracted || !collision.CompareTag("Enemy"))
         {
             return;
         }
-
-        // Set the flag to true to prevent further interactions
         hasInteracted = true;
-
-        // Call the move to enemy function on the player
-        PlayerController playerController = player.GetComponent<PlayerController>();
-        if (playerController != null)
+        
+        if (player != null)
         {
             Debug.Log("voy al enemmigo");
-            playerController.MoveToEnemy(collision.transform.position);
+            //playerController.MoveToEnemy(collision.transform.position);
         }
-
-        // Optionally, destroy the enemy here or do other interactions
         Destroy(collision.gameObject);
     }
 }
