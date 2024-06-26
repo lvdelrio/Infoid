81f0f13d6427044b5c2f862f514ae1b9b8b56b5f
Gonzalo Caballero <gonzalocaballero@me.com>
2024-06-26 10:25:28 -0400

    zoom out camera when speed goes up

--- infoid proyect/Assets/Scripts/CameraController.cs
+++ infoid proyect/Assets/Scripts/CameraController.cs
@@ -2,39 +2,58 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-public GameObject player;
-public float downwardSpeed = 5f;
-public float catchUpMultiplier = 1.2f; 
-public float speedUp = 1.0f; 
+    public GameObject player;
+    public float downwardSpeed = 5f;
+    public float catchUpMultiplier = 1.2f;
+    public float speedUp = 1.0f;
+    public float zoomOutSpeed = 15f;
+    public float zoomInSpeed = 10f;
+    private Camera cam;
 
-void FixedUpdate()
-{
-    if (player != null && player.GetComponent<PlayerController>().CanMove())
+    void Start()
     {
-        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
-        if (playerRigidbody != null)
+        cam = GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        if (player != null && player.GetComponent<PlayerController>().CanMove())
         {
-            float playerSpeed = playerRigidbody.velocity.y;
+            Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
 
-            float cameraBottom = transform.position.y - Camera.main.orthographicSize;
+            if (playerRigidbody != null)
+            {
+                float playerSpeed = playerRigidbody.velocity.y;
+                float cameraBottom = transform.position.y - Camera.main.orthographicSize;
+                float moveInputy = Input.GetAxis("Vertical");
 
-            if (player.transform.position.y < cameraBottom + speedUp || playerSpeed < -downwardSpeed)
-            {
-                float adjustedSpeed = Mathf.Max(downwardSpeed, Mathf.Abs(playerSpeed) / catchUpMultiplier);
-                transform.position += Vector3.down * adjustedSpeed * Time.deltaTime;
-            }
-            else
-            {
-                transform.position += Vector3.down * downwardSpeed * Time.deltaTime;
+                if (player.transform.position.y < cameraBottom + speedUp || playerSpeed < -downwardSpeed)
+                {
+                    float adjustedSpeed = Mathf.Max(downwardSpeed, Mathf.Abs(playerSpeed) / catchUpMultiplier);
+                    transform.position += Vector3.down * adjustedSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    transform.position += Vector3.down * downwardSpeed * Time.deltaTime;
+                }
+
+                if (moveInputy < 0)
+                {
+                    cam.orthographicSize = Mathf.MoveTowards(cam.orthographicSize, 20f, zoomInSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    cam.orthographicSize = Mathf.MoveTowards(cam.orthographicSize, 12f, zoomOutSpeed * Time.deltaTime);
+                }
             }
         }
     }
-}
 
 
 
     public void ResetCameraPosition()
     {
         transform.position = new Vector3(0, 0, -10);
+        cam.orthographicSize = 12f;
     }
 }
